9a814e296a64df1a50ab007bb55493a9292d8e29
polahany <121111159+polahany@users.noreply.github.com>
2025-07-23 07:30:35 +0300

    Merge pull request #1 from polahany/setup/project-theme
    Setup UI theme

---
+++ app/src/main/java/com/example/nectar/ui/theme/Shape.kt
@@ -0,0 +1,14 @@
+package com.example.nectar.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+
+val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(17.dp) ,
+    small = RoundedCornerShape(18.dp),
+    medium = RoundedCornerShape(19.dp) ,
+    large = RoundedCornerShape(bottomStart = 25.dp , bottomEnd = 25.dp) ,
+    extraLarge = RoundedCornerShape(topStart = 15.dp , topEnd = 15.dp) ,
+)
--- app/src/main/java/com/example/nectar/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nectar/ui/theme/Theme.kt
@@ -20,24 +20,13 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = Pink40,
 )
-
 @Composable
 fun NectarTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -45,7 +34,6 @@ fun NectarTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
@@ -53,6 +41,7 @@ fun NectarTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
