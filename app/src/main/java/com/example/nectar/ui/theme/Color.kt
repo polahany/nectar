96705613017c45a2b45fe6751d6093012248c096
polahany <pola.hany.fayez@gmail.com>
2025-07-23 07:28:55 +0300

    Setup UI theme

--- app/src/main/java/com/example/nectar/ui/theme/Color.kt
+++ app/src/main/java/com/example/nectar/ui/theme/Color.kt
@@ -9,3 +9,57 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val mainGreen = Color(83, 177, 117)
+
+val onSplashScreen = Color.White
+
+val onBoardingSmallSlogun = Color(252, 252, 252,179)
+
+val HomeScreenLocation = Color(76, 79, 77)
+
+val searchBar = Color(242, 243, 242)
+
+val secondaryText = Color(124, 124, 124)
+
+val searchIcon = Color(24, 27, 25)
+
+val myMainBackGround = Color(237, 241, 235)
+
+val exuclusivesOffer = Color(3, 3, 3)
+
+val mainBlack = Color(24, 23, 37)
+
+val itemBackGround = Color(242, 243, 242)
+
+val theMinusSign = Color(179, 179, 179)
+
+val lineBreaker = Color(226, 226, 226, 179)
+
+val nutritionsCard = Color(235, 235, 235)
+
+val stars = Color(243, 96, 63)
+
+val cancelIcon = Color(197, 197, 197)
+
+val darkerGreen = Color(72, 158, 103)
+
+
+//categories
+
+val categoriesWithOnlyBody = listOf<Color>(
+    Color(248, 164, 76) ,
+    mainGreen
+)
+
+val categoryColorsBodyOnBoarder = listOf<Pair<Color , Color>>(
+    Color(83, 177, 117, 26) to Color(83, 177, 117, 179),     // 0.1 and 0.7 alpha
+    Color(248, 164, 76, 26) to Color(248, 164, 76, 179),
+    Color(247, 165, 147, 64) to Color(247, 165, 147, 255),
+    Color(211, 176, 224, 64) to Color(211, 176, 224, 255),
+    Color(253, 229, 152, 64) to Color(253, 229, 152, 255),
+    Color(183, 223, 245, 64) to Color(183, 223, 245, 255),
+    Color(131, 106, 246, 38) to Color(131, 106, 246, 128),   // 0.15 and 0.5
+    Color(215, 59, 119, 38) to Color(215, 59, 119, 128)
+)
+
